b996fb7d0a18a5e168f6b83eee033b28aed50cbc
Sood <Karn.Sood@shell.com>
2018-08-27 18:42:12 -0500

    Marketplace working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,116 +1,133 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import '../styles/app.css'
+import "../stylesheets/app.css";
 
 // Import libraries we need.
-import { default as Web3 } from 'web3'
+import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
+const ipfsAPI = require('ipfs-api');
+const ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
+
 // Import our contract artifacts and turn them into usable abstractions.
-import metaCoinArtifact from '../../build/contracts/MetaCoin.json'
+import market_place_artifacts from '../../build/contracts/MarketPlace.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-const MetaCoin = contract(metaCoinArtifact)
+var MarketPlace = contract(market_place_artifacts);
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-let accounts
-let account
+var reader;
 
-const App = {
-  start: function () {
-    const self = this
+window.App = {
+ start: function() {
+  var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider)
+  MarketPlace.setProvider(web3.currentProvider);
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
-      }
-
-      if (accs.length === 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
-        return
-      }
-
-      accounts = accs
-      account = accounts[0]
-
-      self.refreshBalance()
-    })
-  },
-
-  setStatus: function (message) {
-    const status = document.getElementById('status')
-    status.innerHTML = message
-  },
-
-  refreshBalance: function () {
-    const self = this
-
-    let meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.getBalance.call(account, { from: account })
-    }).then(function (value) {
-      const balanceElement = document.getElementById('balance')
-      balanceElement.innerHTML = value.valueOf()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error getting balance; see log.')
-    })
-  },
-
-  sendCoin: function () {
-    const self = this
-
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-
-    this.setStatus('Initiating transaction... (please wait)')
-
-    let meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.sendCoin(receiver, amount, { from: account })
-    }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
-    })
+  if($("#product-details").length > 0) {
+    let productId = new URLSearchParams(window.location.search).get('id');
+    renderProductDetails(productId);
+  } else {
+    renderStore();
   }
+
+  $("#product-image").change(function(event) {
+    const file = event.target.files[0]
+    reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+  });
+
+  $("#add-item-to-store").submit(function(event) {
+    const req = $("#add-item-to-store").serialize();
+    let params = JSON.parse('{"' + req.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+    let decodedParams = {}
+    Object.keys(params).forEach(function(v) {
+      decodedParams[v] = decodeURIComponent(decodeURI(params[v]));
+    });
+    console.log(decodedParams);
+    saveProduct(decodedParams);
+    event.preventDefault();
+  });
+
+  $("#buy-now").submit(function(event) {
+    var sendAmount= $("#buy-now-price").val();
+    var productId = $("#product-id").val();
+    MarketPlace.deployed().then(function(i) {
+      i.buy(productId, {value: sendAmount, from: web3.eth.accounts[0], gas: 440000}).then( function() {
+        $("#msg").show();
+        $("#msg").html("You have successfully purchased the product!");
+      })
+    });
+    event.preventDefault();
+  });
+ }
+};
+
+function renderProductDetails(productId) {
+  MarketPlace.deployed().then(function(f) {
+    f.getProduct.call(productId).then(function(p) {
+      $("#product-name").html(p[1]);
+      $("#product-image").html("<img width='100' src='http://localhost:8080/ipfs/" + p[3] + "' />");
+      $("#product-price").html(displayPrice(p[6]));
+      $("#product-id").val(p[0]);
+      $("#buy-now-price").val(p[6]);
+    });
+  })
 }
 
-window.App = App
+function saveProduct(product) {
+  MarketPlace.deployed().then(function(f) {
+    return f.addProductToStore(product["product-name"], product["product-category"], "imageLink", "descLink", Date.parse(product["product-start-time"]) / 1000, web3.toWei(product["product-price"], 'ether'), product["product-condition"], {from: web3.eth.accounts[0], gas: 4700000});
+  }).then(function(f) {
+    alert("Product added to store!");
+  });
+}
 
-window.addEventListener('load', function () {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn(
-      'Using web3 detected from external source.' +
-      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
-      ' ensure you\'ve configured that source properly.' +
-      ' If using MetaMask, see the following link.' +
-      ' Feel free to delete this warning. :)' +
-      ' http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider)
-  } else {
-    console.warn(
-      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
-      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
-      ' Consider switching to Metamask for development.' +
-      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
-  }
 
-  App.start()
-})
+function renderStore() {
+  // Get the product count
+  // Loop through and fetch all products by id
+  var instance
+
+  return MarketPlace.deployed().then(function(f) {
+    instance = f;
+    return instance.productIndex.call();
+  }).then(function(count){
+    for(var i=1; i<= count; i++) {
+      renderProduct(instance, i);
+    }
+  });
+}
+
+function renderProduct(instance, index) {
+  instance.getProduct.call(index).then(function(f) {
+    let node = $("<div/>");
+    console.log(f);
+    node.addClass("col-sm-3 text-center col-margin-bottom-1 product");
+    node.append("<img src='http://localhost:8080/ipfs/" + f[3] + "' />");
+    node.append("<div class ='title'>" + f[1] + "</div>");
+    node.append("<div> Price: " + displayPrice(f[6]) +"</div>");
+    node.append("<a href='product.html?id=" + f[0] + "'>Details</div>");
+    if (f[8] === '0x0000000000000000000000000000000000000000') {
+      $("#product-list").append(node);
+    } else {
+      $("#product-purchased").append(node);
+    }
+  });
+}
+
+function displayPrice(amt) {
+  return "&Xi;" + web3.fromWei(amt, 'ether');
+}
+
+window.addEventListener('load', function() {
+ // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+ if (typeof web3 !== 'undefined') {
+  console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+  // Use Mist/MetaMask's provider
+  window.web3 = new Web3(web3.currentProvider);
+ } else {
+  console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+  // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+  window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+ }
+
+ App.start();
+});
